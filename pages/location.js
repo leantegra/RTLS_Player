9799b116ddc97598abab61da621a00bb51aaaf10
Dmytro Dogadailo <dimad@leantegra.com>
2017-07-10 11:45:10 +0300

    More tracks with lp=0.3

--- pages/location.js
+++ pages/location.js
@@ -11,12 +11,11 @@ let locations = [
 
 // FIX: load from server
 let sessionIds = [
-  // 'temp.json',
-  // 'dynamic_3wb_tx5_adv500_first.json',
+  'dynamic_3wb_tx5_adv500_first.json',
   // 'dynamic_3wb_tx5_adv500_first_n2.9_sh1.5_C1.3A-8.json',
-  // 'dynamic_3wb_tx5_adv500_first_A55_n3.2_sh1.5.json',
+  'dynamic_3wb_tx5_adv500_first_A55_n3.2_sh1.5.json',
 
-  
+
   //'dynamic_3wb_tx5_adv500_first_n4_sh1.5.json',
   //'dynamic_3wb_tx5_adv500_first_lp0.3_Adivrssi.json',
   //'dynamic_3wb_tx5_adv500_first_lp0.3_Adivrssi1.2.json',
@@ -48,9 +47,10 @@ let sessionIds = [
   // 'dynamic_3wb_tx5_adv100_first_lp0.1_A28_n5.34.json',
   // 'dynamic_3wb_tx5_adv100_first_lp0.1_A48_n2.8.json',
   
-  'dynamic_T_3wb_tx5_adv500.json', // base
-  'dynamic_T_3wb_tx5_adv500_A55_n3.2_sh1.5.json',
-  'dynamic_T_3wb_tx5_adv500_n2.9_sh1.5_C1.3A-10.json', //+++
+  // 'dynamic_T_3wb_tx5_adv500.json', // base
+  // 'dynamic_T_3wb_tx5_adv500_A55_n3.2_lp0.3_sh1.5.json',
+  // 'dynamic_T_3wb_tx5_adv500_A55_n3.2_sh1.5.json',
+  //'dynamic_T_3wb_tx5_adv500_n2.9_sh1.5_C1.3A-10.json', //+++
   // 'dynamic_T_3wb_tx5_adv500-lp0.2.json',
   
   // 'dynamic_T_3wb_tx5_adv500-lp0.3.json',
@@ -60,6 +60,9 @@ let sessionIds = [
   //'dynamic_T_3wb_tx5_adv500_lp0.2_h10_sh1.5.json',
   // user in same place
   //'4wr_spin_0607.json',
+  'temp.json'
+
+  
 ]
 
 async function loadMeta(slug) {
