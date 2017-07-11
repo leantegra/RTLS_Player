88f53a7dcef154d55909bf213ecb06fc2fc272eb
Dmytro Dogadailo <dimad@leantegra.com>
2017-07-11 18:09:08 +0300

    Added session device list.

--- components/session-list.js
+++ components/session-list.js
@@ -2,9 +2,16 @@ import { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { SvgTest } from './track'
 
-import { Headline, List, ListItem, Icon, IconToggle, RadioGroup } from 'react-mdc-web'
+import { Headline, List, ListItem, ListGroup, ListHeader, Icon, IconToggle, RadioGroup, Radio } from 'react-mdc-web'
 
-let DeviceList = ({data}) => (<div>{data.length}</div>)
+function DeviceList({ data }) {
+  let macs = data[0] &&  data[0].devices.map(d => d.id).sort() || []
+  return (
+    <List>
+      {macs.map(mac => (<ListItem>{mac}</ListItem>))}
+    </List>
+  )
+}
 
 class SessionListItem extends PureComponent {
 
@@ -15,9 +22,9 @@ class SessionListItem extends PureComponent {
     data: []
   }
 
-  async fetchSession () {
-    let {location, file} = this.props
-    let {loading, loaded, data} = this.state
+  async fetchSession() {
+    let { location, file } = this.props
+    let { loading, loaded, data } = this.state
     console.log('fetchSession', location, file, loading)
     try {
       data = await fetch(`/static/locations/${location.slug}/${file}`).then(r => r.json())
@@ -25,26 +32,24 @@ class SessionListItem extends PureComponent {
     } finally {
       loading = false
     }
-    this.setState({loading, loaded, data})
+    this.setState({ loading, loaded, data })
   }
 
-  toggle () {
-    let {expanded, loading, loaded, data} = this.state
-    this.setState({expanded: !expanded})
+  toggle() {
+    let { expanded, loading, loaded, data } = this.state
+    this.setState({ expanded: !expanded })
     if (!loaded && !loading) this.fetchSession()
   }
 
   render() {
-    let {file} = this.props;
-    let {expanded, data} = this.state
+    let { file } = this.props;
+    let { expanded, data } = this.state
     return (
-      <ListItem>
+      <ListHeader>
         {file}
-        <IconToggle className="material-icons" onClick={() => this.toggle()}>
-          {expanded ? 'expand_less' : 'expand_more'}
-        </IconToggle>
+        <Icon name={expanded ? 'expand_less' : 'expand_more'} onClick={() => this.toggle()} />
         {expanded && data.length ? <DeviceList data={data} /> : ''}
-      </ListItem>
+      </ListHeader>
     )
   }
 }
@@ -61,11 +66,11 @@ export default class SessionList extends PureComponent {
     return (
       <div>
         <Headline>RTLS sessions</Headline>
-        <List>
+        <ListGroup>
           {files.map(file => (
             <SessionListItem location={location} file={file} key={file} />
           ))}
-        </List>
+        </ListGroup>
       </div>
     )
   }
