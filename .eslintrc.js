031d163a4dbe34ee0439ac06f06c352a55c1061a
Dmytro Dogadailo <dimad@leantegra.com>
2017-07-12 17:53:07 +0300

    Added class-properties support to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
     "extends": ["standard", "standard-react"],
+    "parser": "babel-eslint",
     "rules": {
       "react/react-in-jsx-scope": 0,
       "jsx-quotes": ["error", "prefer-single"],
