cc0a3be85f495d0b44075485659e5b7d547e6698
Dmytro Dogadailo <dimad@leantegra.com>
2017-07-13 14:54:44 +0300

    Add remove track function

--- components/player.js
+++ components/player.js
@@ -1,6 +1,6 @@
 import { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import {List, ListItem, Button} from 'react-mdc-web'
+import { List, ListItem, Button, Icon } from 'react-mdc-web'
 import Track from './track'
 import Timer from './timer'
 import debuger from 'debug'
@@ -10,11 +10,11 @@ const debug = debuger('sessions')
 const COLORS = ['red', 'green', 'purple', 'orange', 'navy', 'deeppink', 'brown', 'magenta', 'indigo', 'black']
 const PLAYER_PADDING = 24
 
-function getTrackColor (index) {
+function getTrackColor(index) {
   return COLORS[index] || COLORS[COLORS.length - 1]
 }
 
-function PlayerCanvas ({meta, tracks, time, tail}) {
+function PlayerCanvas({ meta, tracks, time, tail }) {
   let style = {
     width: meta.width,
     height: meta.height,
@@ -29,20 +29,33 @@ function PlayerCanvas ({meta, tracks, time, tail}) {
         <Track key={i} width={meta.width} height={meta.height}
           points={t.points} color={getTrackColor(i)} start={start} end={time}
         />
-        )
+      )
       )}
     </div>
   )
 }
 
-function PlayerLegend ({tracks}) {
+function PlayerLegend({ tracks, onTrackChange }) {
   return (
-    <List dense>
+    <List dense style={{maxWidth: 600}}>
       {
         tracks.map((t, i) => (
-          <ListItem key={i} style={{color: getTrackColor(i)}}>{t.id} ({t.file})</ListItem>
+          <ListItem key={i}>
+            <span style={{ color: getTrackColor(i) }}>{t.id} ({t.file})</span>
+            <Icon className='clear-icon' name='clear' onClick={() => onTrackChange({track: t, checked: false})} />
+          </ListItem>
         ))
       }
+      <style jsx>{`
+        // demo of style-jsx problem
+        List, ul {
+          maxWidth: 600px;
+        }
+        .clear-icon {
+          color: red;
+          cursor: pointer;
+        }
+      `}</style>
     </List>
   )
 }
@@ -59,12 +72,12 @@ export default class Player extends PureComponent {
     tail: 0
   }
 
-  onTick = (time, tail) => this.setState({time, tail})
+  onTick = (time, tail) => this.setState({ time, tail })
 
   clear = () => this.props.removeAllTracks()
 
-  render () {
-    let { meta, tracks } = this.props
+  render() {
+    let { meta, tracks, onTrackChange } = this.props
     let { time, tail } = this.state
     debug(`Player time=${time}, tail=${tail}`, tracks)
     if (!meta) return null
@@ -75,8 +88,8 @@ export default class Player extends PureComponent {
       <div>
         <PlayerCanvas meta={meta} tracks={tracks} time={time} tail={tail} />
         <Timer onTick={this.onTick} max={maxTime} width={meta.width + 2 * PLAYER_PADDING} />
-        <PlayerLegend tracks={tracks} />
-        {tracks.length ? <Button onClick={ this.clear }>Remove all</Button> : null }
+        <PlayerLegend tracks={tracks} onTrackChange={onTrackChange} />
+        {tracks.length ? <Button onClick={this.clear}>Remove all</Button> : null}
       </div>
     )
   }
